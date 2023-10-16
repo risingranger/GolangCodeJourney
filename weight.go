20eeb2a1b060f5b3dfe179c1bea2fedab8b52f9a
risingranger <96188342+risingranger@users.noreply.github.com>
2023-10-16 13:29:45 +0530

    Create weight.go

---
+++ weight.go
@@ -0,0 +1,9 @@
+package weight
+
+func KilogramsToPounds(kilograms float64) float64 {
+	return kilograms * 2.20462
+}
+
+func PoundsToKilograms(pounds float64) float64 {
+	return pounds / 2.20462
+}
