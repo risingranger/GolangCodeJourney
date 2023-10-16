27971ebd3ac3542d3f474c16bd7d893bd733035d
risingranger <risingranger987@gmail.com>
2023-10-16 15:32:44 +0530

    Deleted files

--- temperature.go
+++
@@ -1,9 +0,0 @@
-package temperature
-
-func CelsiusToFahrenheit(celsius float64) float64 {
-	return (celsius * 9/5) + 32
-}
-
-func FahrenheitToCelsius(fahrenheit float64) float64 {
-	return (fahrenheit - 32) * 5/9
-}
--- weight.go
+++
@@ -1,9 +0,0 @@
-package weight
-
-func KilogramsToPounds(kilograms float64) float64 {
-	return kilograms * 2.20462
-}
-
-func PoundsToKilograms(pounds float64) float64 {
-	return pounds / 2.20462
-}
