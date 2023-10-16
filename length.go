cf24ea69969166be9528d27efc6471656c500d4c
risingranger <96188342+risingranger@users.noreply.github.com>
2023-10-16 13:29:18 +0530

    Create length.go

---
+++ length.go
@@ -0,0 +1,9 @@
+package length
+
+func InchesToCentimeters(inches float64) float64 {
+	return inches * 2.54
+}
+
+func CentimetersToInches(centimeters float64) float64 {
+	return centimeters / 2.54
+}
